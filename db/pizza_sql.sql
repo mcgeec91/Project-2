1ae6472af4ccb8a9bbb946c6725e434666d8b88a
suitto321 <suittoscience@gmail.com>
2019-02-02 20:17:37 -0500

    redo database pizza_db

--- db/pizza_sql.sql
+++ db/pizza_sql.sql
@@ -1,9 +1,14 @@
-#DROP DATABASE IF EXISTS PIZZA_DB;
-#CREATE DATABASE PIZZA_DB;
-USE PIZZA_DB;
+#DROP DATABASE IF EXISTS pizza_db;
+#CREATE DATABASE pizza_db;
+#USE pizza_db;
 
 -- create tables 
-DROP TABLE pizza;
-SELECT * FROM pizza_db.8358pizza;
-RENAME TABLE 8358pizza TO pizza;
+#DROP TABLE pizza;
+#SELECT * FROM pizza_db.8358pizza;
+#RENAME TABLE 8358pizza TO pizza;
+#SELECT * FROM pizza_db.data2 where AveragePrice < 1;
+
+-- create new table with pizza_db 
+SELECT * FROM pizza_db.datapizza;
+RENAME TABLE datapizza TO pizza;
 SELECT * FROM pizza_db.pizza;
